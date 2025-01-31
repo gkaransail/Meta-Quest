b168336967e8c67bace33af0987361ab3c2907b1
gkaransail <79159677+gkaransail@users.noreply.github.com>
2025-01-31 09:04:12 -0500

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,7 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    public float speed = 10.0f;
+    public float turnSpeed = 25.0f;
+    public float horizontalInput;
+    public float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,8 +17,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(0,0,1);       Moves forward 
-        transform.Translate(Vector3.forward);     // Vectors3 is the 3D representations of vectors and points 
-                                                  //forward -- 0,0,1
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+
+
+       
+        //  Moves forward 
+        transform.Translate(Vector3.forward * Time.deltaTime*speed*forwardInput);     // Vectors3 is the 3D representations of vectors and points 
+        //transform.Translate(Vector3.right * Time.deltaTime*turnSpeed*horizontalInput);   
+
+        //We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime*turnSpeed*horizontalInput);                                  
     }
 }
